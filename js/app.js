918d713edf1768cfcc0a5c1836e368e6397bf034
framista <adajab1@gmail.com>
2020-03-20 21:34:45 +0100

    Drag and drop question

--- js/app.js
+++ js/app.js
@@ -1,13 +1,49 @@
 const nextButton = document.getElementById("nextButton");
 const previousButton = document.getElementById("previousButton");
 const stepsBar = document.querySelectorAll(".step");
-
-console.log(stepsBar)
+const task = document.querySelector(".task");
+const questions = document.querySelectorAll(".question");
 
 let currentStep = 0;
 
+questions.forEach(question => {
+    question.addEventListener('dragstart', () => {
+        question.classList.add("question--dragging");
+    });
+
+    question.addEventListener('dragend', () => {
+        question.classList.remove("question--dragging");
+    })
+
+})
+
+task.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    const selectedQuestion = document.querySelector(".question--dragging");
+    const nextQuestion = getNextQuestionDiv(e.clientY);
+    if (nextQuestion) {
+        task.insertBefore(selectedQuestion, nextQuestion);
+    } else {
+        task.appendChild(selectedQuestion);
+    }
+
+})
+
+function getNextQuestionDiv(y) {
+    const otherQuestion = [...task.querySelectorAll(".question:not(.question--dragging)")]
+    return otherQuestion.reduce((closest, child) => {
+        const rectangle = child.getBoundingClientRect();
+        const offset = y - rectangle.top - rectangle.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
 nextButton.addEventListener('click', () => {
-    addDoneClass(currentStep)
+    addDoneClass(currentStep);
     currentStep++;
     previousButton.disabled = false;
     if (currentStep === 4) {
@@ -17,8 +53,8 @@ nextButton.addEventListener('click', () => {
 })
 
 previousButton.addEventListener('click', () => {
-    removeDoneClass(currentStep - 1)
     currentStep--;
+    removeDoneClass(currentStep);
     nextButton.innerHTML = "Następne"
     if (currentStep === 0) {
         previousButton.disabled = true;
